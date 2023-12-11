edcb91ed274dc5d29de276e5405625a0fa9da937
vmuliadi-astro <101077841+vmuliadi-astro@users.noreply.github.com>
2023-12-11 22:18:33 +0700

    chore(docs): update readme

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 convert ALL worksheet in XLSX file and then convert it into separated CSV files
 
 ## HOW-TO install
+
 ```
 $ pip3 install -r requirements.txt
 $ python3 xlsx-converter.py XLSX_FILE.xlsx
--- xlsx-converter.py
+++ xlsx-converter.py
@@ -3,15 +3,25 @@ import pandas as pd
 from sys import argv, exit
 
 try:
+  total_data=0
   excel_file = pd.read_excel(argv[1], None)
   for sheet_name in excel_file.keys():
     blacklisted_keywords = ["DONE", "NEED FIX"]
-    if all(keyword not in sheet_name for keyword in blacklisted_keywords):
+    blacklisted_worksheet = ["fix", "DATA", "2"]
+    if (all(keyword not in sheet_name for keyword in blacklisted_keywords)) and (all(keyword not in sheet_name for keyword in blacklisted_worksheet)):
       excel_sheet = pd.read_excel(argv[1], sheet_name)
       sheet_name = sheet_name.split()[-1]
+      total_data = total_data + len(excel_sheet.index.tolist())
       print("{}.csv contains {:d} data".format(sheet_name, len(excel_sheet.index.tolist())))
-      with open("{}.csv".format(sheet_name), "w") as csv_file:
-        csv_file.write(excel_sheet.to_csv())
+      excel_sheet.to_csv("{}.csv".format(sheet_name), index=False)
+
+  data_counting = "Total data in this XLSX: {:d} data".format(total_data)
+
+  print()
+  print("= " + "=" * len(data_counting) + " =")
+  print("= " + data_counting + " =")
+  print("= " + "=" * len(data_counting) + " =")
+  print()
 except Exception as err:
   print(err)
   exit(1)
