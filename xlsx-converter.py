996831914090b039a7560fc92246ba7cf32cacc7
vmuliadi-astro <101077841+vmuliadi-astro@users.noreply.github.com>
2023-12-12 10:05:58 +0700

    chore(converter): clean blacklist data

--- xlsx-converter.py
+++ xlsx-converter.py
@@ -6,14 +6,24 @@ try:
   total_data=0
   excel_file = pd.read_excel(argv[1], None)
   for sheet_name in excel_file.keys():
-    blacklisted_keywords = ["DONE", "NEED FIX"]
-    blacklisted_worksheet = ["fix", "DATA", "2"]
-    if (all(keyword not in sheet_name for keyword in blacklisted_keywords)) and (all(keyword not in sheet_name for keyword in blacklisted_worksheet)):
-      excel_sheet = pd.read_excel(argv[1], sheet_name)
-      sheet_name = sheet_name.split()[-1]
-      total_data = total_data + len(excel_sheet.index.tolist())
-      print("{}.csv contains {:d} data".format(sheet_name, len(excel_sheet.index.tolist())))
-      excel_sheet.to_csv("{}.csv".format(sheet_name), index=False)
+
+    # for example: blacklisted_keywords = ["test", "fix", "done"]
+    # blacklisted_keywords: worksheet name that contains a substring that listed in this list will skipped
+    # any worksheet that contains "test" "fix" or "done" will be skipped
+    blacklisted_keywords = []
+
+    # for example: blacklisted_worksheet = ["6505", "6506"]
+    # blacklisted_worksheet: any worksheet name that contains in this list will be skipped
+    # in this example, worksheet with the name "6505" and "6506" will be skipped
+    blacklisted_worksheet = []
+
+    if (all(keyword not in sheet_name for keyword in blacklisted_keywords)) and \
+      (all(keyword not in sheet_name for keyword in blacklisted_worksheet)):
+        excel_sheet = pd.read_excel(argv[1], sheet_name)
+        sheet_name = sheet_name.split()[-1]
+        total_data = total_data + len(excel_sheet.index.tolist())
+        print("{}.csv contains {:d} data".format(sheet_name, len(excel_sheet.index.tolist())))
+        excel_sheet.to_csv("{}.csv".format(sheet_name), index=False)
 
   data_counting = "Total data in this XLSX: {:d} data".format(total_data)
 
